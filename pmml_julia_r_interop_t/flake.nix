bd6fb4a04c0ca20c87de7d6c7c6e8f8cef63758d
Bruno Rodrigues <brodriguesco@protonmail.com>
2026-05-09 14:15:20 +0200

    chore: lock flake and point to local tlang

--- pmml_julia_r_interop_t/flake.nix
+++ pmml_julia_r_interop_t/flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:rstats-on-nix/nixpkgs/2026-04-02";
     flake-utils.url = "github:numtide/flake-utils";
-    t-lang.url = "github:b-rodrigues/tlang/v0.52.0";
+    t-lang.url = "git+file:///home/brodrigues/Documents/repos/tlang";
   };
 
   nixConfig = {
